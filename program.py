b206913ddb21fd55098737061abe181b03da986f
AtaurRehman <ataulrehmangee994@gmail.com>
2026-07-14 10:51:36 +0500

    taha ka python code

---
+++ program.py
@@ -0,0 +1,12 @@
+
+# print("Hello World!" , end= " ")
+# print("I will print on the same line.")
+
+# print(type(3))
+# print(type("3"))
+# print(358)
+# print(50000)
+
+
+# Print "I am" and the number 25
+print(""""I am" and the number""",25 )
