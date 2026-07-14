89c0ad06774be655a0eac49dc6b163a9b04abfe3
AtaurRehman <ataulrehmangee994@gmail.com>
2026-07-14 14:07:36 +0500

    add function

---
+++ Function.py
@@ -0,0 +1,9 @@
+def taha():
+ a = input("enter the  name :")
+ b = int(input("enter the roll number :"))
+ c = input("enter the school name")
+ print("Name :",a)
+ print("Roll number:",b)
+ print("school name:",c)
+
+taha()
