b53d77596c2fcc463a2959c968ce1f1ab2452d59
AtaurRehman <ataulrehmangee994@gmail.com>
2026-07-14 14:36:55 +0500

    add operation

---
+++ operation.py
@@ -0,0 +1,15 @@
+a = int(input("enter the frist number :"))
+b =int(input("enter the second number :"))
+op = input("enter the operation :")
+if(op == "+"):
+    print(a+b)
+elif(op == "-"):
+    print(a-b)
+elif (op == "*"):
+    print(a*b)
+elif(op == "/"):
+    print(a/b)
+elif(op == "%"):
+    print(a%b)
+else :
+    print("op is not avaible")
