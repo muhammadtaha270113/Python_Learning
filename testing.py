a6620a1faa5fb5e4309d6d8a4c1a899cec785ef3
AtaurRehman <ataulrehmangee994@gmail.com>
2026-07-15 10:58:22 +0500

    add something

---
+++ testing.py
@@ -0,0 +1,39 @@
+# string = "Hello world"
+# print(string[-11:-4])
+
+# listing = [23,(23,16,74),{98,32},32]
+# print(listing[2][1])
+
+# seting = {6,2,9,9,1,0}
+# print(seting[1])
+
+# dicting = {
+#     "name":"taha",
+#     "rollno":52,
+#     "school":"govt islamia high school"
+#     }
+# print(dicting['rollno'])
+
+# list = [123,'abcd',10.2,'d']
+# list1 = ['hello','world']
+
+# print(list*2)  
+
+# dicting = {
+#     "name":"taha",
+#     "rollno":52,
+#     "school":"govt islamia high school"
+#     }
+
+# print(dicting.values())   
+# 
+
+
+# list = [34,23,53,23,53,12,76]
+# list[3]  = "taha"
+# print(list)  
+
+# tuple = (43,53,64,28)
+# tuple[2] = 23
+# print(tuple) 
+
---
+++ variable.py
@@ -0,0 +1,94 @@
+# x = 5
+# print(x)
+# print(type(x))
+
+
+# y = float(x)
+# print(y)
+# print(type(y))  
+
+
+
+# string = 54321
+# print(string)
+# print(type(string))
+   
+# string = str(string)
+# print(string)
+# print(type(string))
+
+
+# int() , float() , str() , list() , tuple() , dict() , set() , bool()
+
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)      
+
+# x = 5
+# y = "John"
+# print(x + y)
+
+
+  
+# x = "awesome"
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+
+# loop
+
+
+# for i in range(5,25,5):
+#     print(i)
+    
+  
+# for i in range(5,25,5):
+#     print("taha is good boy")
+
+# list = ["taha", "saif", "owais", "hammad", "muzammil", "kamran", "shafay", "usama", "khubab"]
+# tuple = (1000,1001,1002,1003,1004,1005,1006,1007,1008,1009,1010)
+# set = {"pizza", "burger", "pasta", "BBQ", "Biryani", "karahi"}
+# dict = {
+#     "name" : "taha",
+#     "age" : 20,
+#     "gender" : "male",
+#     "city" : "karachi"
+# }
+# for taha in list:
+#     print(taha)
+
+# for i in tuple:
+#     print(i)
+
+# for i in set:
+#     print(i)
+
+# for i in dict:
+#     print("key ",i,"values ",dict[i])
+
+# i = 1
+# while i <= 6:
+#   print(i)
+#   i += 1 
+
+i = 1
+while i < 6:
+  if i == 3:
+    i += 1
+    continue   
+  print(i)
+  i += 1
+
+  
