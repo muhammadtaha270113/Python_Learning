cd2c9b09d38005a9b92975f69a58372837d5007b
AtaurRehman <ataulrehmangee994@gmail.com>
2026-07-15 09:11:53 +0500

    add practices

---
+++ practices.py
@@ -0,0 +1,38 @@
+#_Integer
+a = 2
+print(a)
+
+# floating
+b = 3.8
+print(b)
+
+ # string
+c = '100'
+print(c)
+
+ # boolean
+d = True
+print(d)
+ 
+# empty value or null data
+x= None
+print(x)
+
+
+
+# list 
+school_list = [" govt islamia high school",52,"taha"]
+print(school_list)
+# tuple 
+tuple =(True, False)
+print(tuple)
+# Set 
+set = {52,20,22}
+print(set)
+# Dict 
+dict={
+   "username" : "taha",
+   "rollno " : 52,
+   "school name ": "Govt islamia high school"
+}
+print(dict)
