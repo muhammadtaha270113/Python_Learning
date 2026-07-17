fdef32d136a2e0ddcf77db2ac21a7f29dc4b505e
AtaurRehman <ataulrehmangee994@gmail.com>
2026-07-17 11:35:43 +0500

    add programs

---
+++ programming.py
@@ -0,0 +1,36 @@
+# programs
+
+# program 1 
+a =int(input("enter the first number : "))
+b =int(input("enter the second number : "))
+c =int(input("enter the third number : "))
+d =int(input("enter the fouth number : "))
+sum =a+b+c+d
+avg = sum/4
+print("sum =",sum)
+print("avg =", avg)
+
+# program 2
+Name = input("enter the name : ")
+Age =int(input("enter the age : "))
+School_name = input("enter the school name : ")
+print("your Name is :",Name)
+print("your Age is : ",Age)
+print("your School name is : ",School_name)
+
+# program 3
+km =int(input("enter distance in kilometer :"))
+m = km*1000
+print("Distance in meters",m)
+
+# program 4
+height =float(input("enter height in inches :"))
+height_cm =height*2.54
+print("Height in centimeters = ",height_cm)
+
+# program 5
+ctemp =float(input("enter temp in celsius:"))
+ftemp =9.0/5.0 *ctemp+32
+print("Temp. in Fahrenheit =",ftemp)
+
+
